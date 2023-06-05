a83395d8c7a739a1ff0f2a6b7bb647bc237b68c3
Basel Serhan <baselserhan1993@gmail.com>
2023-06-05 15:20:52 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,36 @@ import { connect } from "react-redux";
 import moment from "moment";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import logo from './reminders-icon.png';
+import logo from "./reminders-icon.png";
 import { add_Reminder, remove_Reminder, clear_Reminder } from "../actions";
 
 class App extends Component {
+  state = {
+    text: "",
+    date: new Date(),
+  };
 
-    state = {
-        text: '',
-        date: new Date()
-    }
-
-    render_Reminders() {
-      const {reminders} = this.props;
-      return (
-        <ul className="list-group">
-          {
-            reminders.map(reminder => {
-              return (
-                <li key={reminder.id} className="list-group-item">
-                  <div>{reminder.text}</div>
-                  <div>{moment(new Date(reminder.date)).fromNow()}</div>
-                  <div className="closeIcon btn btn-danger" onClick={() => this.props.remove_Reminder(reminder.id)}>X</div>
-                </li>
-              )
-            })
-          }
-        </ul>
-      )
-    }
+  render_Reminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group">
+        {reminders.map((reminder) => {
+          return (
+            <li key={reminder.id} className="list-group-item">
+              <div>{reminder.text}</div>
+              <div>{moment(new Date(reminder.date)).fromNow()}</div>
+              <div
+                className="closeIcon btn btn-danger"
+                onClick={() => this.props.remove_Reminder(reminder.id)}
+              >
+                X
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
   render() {
     console.log(this.props);
     return (
@@ -42,33 +44,38 @@ class App extends Component {
         <input
           className="form-control"
           type="text"
+          required
           value={this.state.text}
           placeholder="Enter What U Think ... ?"
           onChange={(e) => this.setState({ text: e.target.value })}
         />
         <DatePicker
           className="form-control"
+          required
           value={this.state.date}
           placeholderText="Enter Date"
           selected={this.state.date}
-          onChange={(date) => this.setState({date})}
+          onChange={(date) => this.setState({ date })}
           showTimeSelect
           timeFormat="HH:mm"
           timeCaption="time"
           dateFormat="MMMM d, yyyy h:mm aa"
         />
-        <button onClick={() => {
-          this.props.add_Reminder(this.state.text, this.state.date)
-          this.setState({ text: '', date: '' })
-          }} 
-          className="btn btn-primary w-100">
+        <button
+          onClick={() => {
+            this.props.add_Reminder(this.state.text, this.state.date);
+            this.setState({ text: "", date: "" });
+          }}
+          className="btn btn-primary w-100"
+        >
           Add Reminder
-          </button>
+        </button>
         {this.render_Reminders()}
         <button
-        onClick={() => this.props.clear_Reminder()}
-        className="btn btn-danger w-100">
-        Clear Reminder
+          onClick={() => this.props.clear_Reminder()}
+          className="btn btn-danger w-100"
+        >
+          Clear Reminder
         </button>
       </div>
     );
@@ -87,13 +94,15 @@ class App extends Component {
 //     }
 // } // equivalent to: state => {return {reminders: state}}
 
-export default connect(state => {
-  return {
-    reminders: state
+export default connect(
+  (state) => {
+    return {
+      reminders: state,
+    };
+  },
+  {
+    add_Reminder,
+    remove_Reminder,
+    clear_Reminder,
   }
-}, {
-  add_Reminder,
-  remove_Reminder,
-  clear_Reminder
-}
 )(App);
